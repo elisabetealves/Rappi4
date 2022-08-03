976de3d16dcab4f830b84c49c46d51af79e00a70
Liz alves <elisabete_carente@hotmail.com>
2022-08-03 13:38:18 -0300

    add as pasta pages com as paginacao do projeto

---
+++ src/pages/errorPage/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ErrorPageContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+height: 100vh;
+background-color: #E86E5A;
+`
+
+export const ErrorImage = styled.img`
+   width: 100vw;
+   max-width: 900px;
+   margin-top: -60px;
+`
